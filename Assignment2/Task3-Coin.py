db6299f80d32fe9dff4a99daa4bfefb596996d48
Siren Melkevig <sirenmelkevig@Sirens-MBP-2.home>
2024-02-28 19:36:44 +0100

    Last changes

--- Assignment2/Task3-Coin.py
+++ Assignment2/Task3-Coin.py
@@ -1,3 +1,4 @@
+import time
 #Task 3.1
 def gready_coin(N, coins):
     coins.sort(reverse=True)
@@ -9,34 +10,35 @@ def gready_coin(N, coins):
             count = N // coin
             total_coins += count
             total_amount += count
-            for i in range(total_coins):
+            for _ in range(total_coins):
                 result.append(coin)
             N -= count * coin
         total_coins = 0
     return f"Coins: {result}, total of {total_amount}"
 
-
+start_time = time.time()
 coins = [1, 5, 11]
-
 print(gready_coin(15, coins))
-
+end_time = time.time()
+execution_time = (end_time - start_time) * 1000
+print(execution_time)
 
  #Task 3.2 
-def optimize_gready_coin(N,coins):
+def optimize_coin(N,coins):
     coins.sort(reverse=True)
     results = [float("inf")] * (N+1)
     results[0] = 0
-    coinsss = {}
     
     for i in range(1, N+1):
         for coin in coins:
             if coin <= i:
                 q = min(results[i], results[i - coin]+1)
-                coinsss[q] = str(coin)
-                print(coinsss)
                 results[i] = q
     return results[N]
 
+start_time = time.time()
 coins = [1,5,11]
-print("VÅR")
-print(optimize_gready_coin(15, coins))
+print(optimize_coin(15, coins))
+end_time = time.time()
+execution_time = (end_time - start_time) * 1000
+print(execution_time)
